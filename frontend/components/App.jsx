0e7c5960b62460be3a8ea1b3fff0de30c4af7a7d
jdcapili <jdcapili12@gmail.com>
2019-10-01 18:13:27 -0400

    greeting container done

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import GreetingContainer from './greeting/greeting_container'
 
 const App = () => (
   <div>
     <h1>S N A C C</h1>
+    <GreetingContainer />
   </div>
 )
 
---
+++ frontend/components/greeting/greeting.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class Greeting extends React.Component{
+  
+  render(){
+    const currentUser = this.props.currentUser;
+
+    if(currentUser){
+      return(
+        <div>
+          <h2>{currentUser.display_name}</h2>
+          <button onClick={this.props.logout}>Logout</button>
+        </div>
+      )
+    } else {
+      return(
+        <div>
+          <Link to="/signup">Sign Up</Link>
+          <Link to="/login">Sign In</Link>
+        </div>
+      )
+    }
+  }
+}
+
+export default Greeting;
