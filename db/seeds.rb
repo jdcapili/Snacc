70ca57cc0d85ed8108b20c84fc9339b463f28576
jdcapili <jdcapili12@gmail.com>
2019-10-08 18:18:52 -0400

    seed file update v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
 Channel.destroy_all
-Channeluser.destroy_all
+ChannelUser.destroy_all
 Message.destroy_all
 
 demo = User.create(display_name: 'DemoUser', email: 'demo@user.com', password:'123456' )
