c189b3f77a09830a184fe22cc83e73254a0d162c
jdcapili <jdcapili12@gmail.com>
2023-03-14 16:16:02 -0400

    Set initialize on precompile to false

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,9 @@ Bundler.require(*Rails.groups)
 module Snacc
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
+
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
